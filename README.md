4180a60e55120dee78372516bdf130bdadef539c
eduardo reta rj <er031991@gmail.com>
2019-11-19 19:27:40 -0500

    final product for now

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GifTastic
+In search of watching some car gifs. Just type in a car to see what comes up.
+By filling in the input and pressing the add car button.
+You can add more buttons.
+By pressing the buttons up top; your page will be uploaded with  related GIFS.
+To start watching the GIFS just simply press on the one you want to watch or stop it from playing 
+if your tired of it looping.
+ 
--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,90 +1,99 @@
-$(document).ready(function() {
-    // //Javascript
-    var exampleCars = ["evo lancer", "rx7", "gtr", "supra", "nissan silvia", "370z", "miata", "gti", "bmw E30"]
-// use api to grab GIFS corresponding to a car search
-   
-       
-     function firstCar() {
+$(document).ready(function () {
+     //Javascript + JQUERY
+
+    var exampleCars = ["Mustang", "Evo Lancer", "Rx7", "G.T.R.", "Supra", "Nissan Silvia", "370z", "Miata", "GTI", "BMW E30"]
+    // Use api to grab GIFS corresponding to a car search
+
+    //This function is for the initial page, so gifs will be loaded on the page.
+    function firstCar() {
+
         var mustang = "https://api.giphy.com/v1/gifs/search?q=mustang&api_key=gBpiXvp8GYVz38NiBBNHJjLI7D3P2pCF&limit=9&rating=PG-13&lang=en";
-     $.ajax({
-            url: mustang,
-            method: "GET"
-        })
-        .then(function (response) {
+        $.ajax({
+                url: mustang,
+                method: "GET"
+            })
+            .then(function (response) {
 
-             var results = response.data;
+                var results = response.data;
 
-             for (var i = 0; i < results.length; i++) {
+                //loop through the results to find  each gif image
+                for (var i = 0; i < results.length; i++) {
 
-             var gifDiv = $("<div>");
+                    var gifDiv = $("<div>");
 
-             var carImage = $("<img>");
-             // var rating = results[i].rating;
-             var p = $("<p>").text("Rating:" + response.data[0].rating);
+                    var carImage = $("<img>");
 
-             carImage.attr("src", results[i].images.fixed_height.url);
-             carImage.addClass("gifAdded");
-           
-             gifDiv.append(carImage);
+                    var p = $("<p class='rating'>").text("Rating:" + response.data[0].rating);
 
-             gifDiv.append(p);
+                    carImage.attr("src", results[i].images.original_still.url);
 
-             $("#gifs").prepend(gifDiv);
-          
-        }
-        });
-        
-     }
-    
-     firstCar();
+                    carImage.attr("id", results[i].id)
+
+                    carImage.addClass("gifAdded");
+
+                    gifDiv.append(carImage);
+
+                    gifDiv.append(p);
+
+                    $("#gifs").prepend(gifDiv);
+
+                };
+                
+                 //Function to play and stop gif
+             playGif(results);
+            });
+    };
+
+    firstCar();
+
+    //This function  interacts with each button pressed supplying the appropriate gifs.
     function carGifs() {
         $("#gifs").empty();
-        
+
         var carSearch = $(this).attr("data-car");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + carSearch + "&api_key=gBpiXvp8GYVz38NiBBNHJjLI7D3P2pCF&limit=9&rating=PG-13&lang=en";
 
-       //ajax call to the api grabing results and adding the exampleCars
+        //ajax call to the api grabing results and adding the exampleCars
         $.ajax({
                 url: queryURL,
                 method: "GET"
             })
             .then(function (response) {
 
-               console.log(response.data);
-
+                console.log(response.data);
+                let paused = true;
                 var results = response.data;
-        
+
                 //loop the each result to add tags 
                 for (var i = 0; i < results.length; i++) {
-                    
 
                     var gifDiv = $("<div>");
 
                     var carImage = $("<img>");
-                    // var rating = results[i].rating;
-                    var p = $("<p>").text("Rating:" + response.data[0].rating);
+                    
+                    var paragraph = $("<p class='rating'>").text("Rating:" + response.data[0].rating);
+                    
+                    carImage.attr("id", results[i].id);
 
-                    carImage.attr("src", results[i].images.fixed_height.url);
-                    carImage.addClass("gifAdded");
+                    carImage.attr("src", results[i].images.original_still.url);
                   
+                    carImage.addClass("gifAdded");
+
                     gifDiv.append(carImage);
 
-                    gifDiv.append(p);
+                    gifDiv.append(paragraph);
 
                     $("#gifs").prepend(gifDiv);
-                    
-                
-
                 };
-                $(".gifAdded").on("click", function(){
-                    console.log(this)
-                    $(".gifAdded").stop()
-                })
+
+                //Function to play and stop gif
+                playGif(results);
+                
             });
-            
+
     };
 
-    
+
     //renders buttons to the screen
     function renderButtons() {
 
@@ -101,24 +110,24 @@ $(document).ready(function() {
             makeButtons.text(exampleCars[e]);
 
             $("#buttons").append(makeButtons);
-            
+
         };
     };
-    
+
     $(document).on("click", ".car", carGifs);
-    
+
     renderButtons();
-//adds button on click and validates each input to see if it already exist if it doesnt it adds it 
+    //adds button on click and validates each input to see if it already exist if it doesnt it adds it 
     $("#add-car").on("click", function (event) {
 
         event.preventDefault();
 
         var input = $("#car-input").val().trim();
-        
+
         var existingCar = "";
 
         for (var i = 0; i < exampleCars.length; i++) {
-            if(exampleCars[i] === input) {
+            if (exampleCars[i] === input) {
                 existingCar = exampleCars[i];
             };
         };
@@ -130,6 +139,31 @@ $(document).ready(function() {
             renderButtons();
         };
     });
+    ////Every click on the gif it will determine which gif is loaded either the  moving or paused one.
+    function playGif(results) {
+        $(".gifAdded").on("click", function () {
+
+            for (var i = 0; i < results.length; i++) {
+
+                if (results[i].id === this.id) {
+
+                    if (this.src === results[i].images.original_still.url) {
+                        this.src = results[i].images.fixed_height.url;
+                        break;
+                    };
+                    if (this.src === results[i].images.fixed_height.url) {
+                        this.src = results[i].images.original_still.url
+                        break;
+                    };
+
+                };
+            };
+        });
+    }
+
+
+
+
 
 });
 
